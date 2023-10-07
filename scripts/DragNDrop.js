eb660d2517866bf1e427d3e510f32cf38efb0a8d
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-10-07 17:15:23 +0200

    update

--- scripts/DragNDrop.js
+++ scripts/DragNDrop.js
@@ -21,7 +21,7 @@ function drag(e)
 }
 
 
-function drop(e)
+/*function drop(e)
 {
     e.preventDefault();
     clone=e.target.cloneNode(true);
@@ -54,38 +54,52 @@ function drop(e)
         console.log(clone.id)
 
 
-       console.log(sonoAdiacenti("div-cell-"+dragindex,clone.id));
+
     }
 
+}*/
+
+function drop(e) {
+    e.preventDefault();
+    clone = e.target.cloneNode(true);
+
+    let data = e.dataTransfer.getData("text"); //id del trapel che sposto
+
+    if (clone.id !== data) {
+        console.log("DIVERSO");
+
+        let nodelist = document.getElementById("parent").childNodes;
+
+        for (let i = 0; i < nodelist.length; i++) {
+            if (nodelist[i].id == data) {
+                dragindex = i;
+                break;
+            }
+        }
+
+        if (areAdjacent(document.getElementById(data), e.target)) {
+            document.getElementById("parent").replaceChild(document.getElementById(data), e.target);
+
+            document.getElementById("parent").insertBefore(clone, document.getElementById("parent").childNodes[dragindex]);
+            console.log(dragindex);
+            console.log("conseguente id sarà " + "div-cell-" + dragindex);
+            console.log(clone.id);
+        } else {
+            console.log("I div non sono adiacenti!");
+        }
+    }
+}
+
+function areAdjacent(div1, div2) {
+    const index1 = Array.from(div1.parentNode.children).indexOf(div1);
+    const index2 = Array.from(div2.parentNode.children).indexOf(div2);
+
+    return Math.abs(index1 - index2) === 1 || Math.abs(index1 - index2) === 4;
 }
 
 
-function sonoAdiacenti(div1Id, div2Id) {
-    var div1 = document.getElementById(div1Id);
-    var div2 = document.getElementById(div2Id);
 
-    // Ottieni le coordinate
-    var rect1 = div1.getBoundingClientRect();
-    var rect2 = div2.getBoundingClientRect();
 
-    // Controlla se sono adiacenti orizzontalmente
-    if (
-        (rect1.top >= rect2.top && rect1.top <= rect2.bottom) ||
-        (rect1.bottom >= rect2.top && rect1.bottom <= rect2.bottom)
-    ) {
-        return "Orizzontale";
-    }
-
-    // Controlla se sono adiacenti verticalmente
-    if (
-        (rect1.left >= rect2.left && rect1.left <= rect2.right) ||
-        (rect1.right >= rect2.left && rect1.right <= rect2.right)
-    ) {
-        return "Verticale";
-    }
-
-    return "Non adiacenti";
-}
 
 
 
